39a1492973d152bdca87137108804ff0a91c2a59
ednaldomarinho <ednaldosmarinho@gmail.com>
2020-10-06 16:33:21 +0000

    Lesson 11 - Creating ACL Logic

--- app/User.php
+++ app/User.php
@@ -2,9 +2,11 @@
 
 namespace App;
 
+use App\Role;
+use App\Permission;
+use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
@@ -36,4 +38,27 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function roles()
+    {
+        $this->belongsToMany(Role::class);
+    }
+    
+    
+    public function hasPermission(Permission $permission)
+    {
+        return $this->hasAnyRoles($permission->roles);
+    }
+
+    public function hasAnyRoles($roles)
+    {
+       if (is_array($roles) || is_object($roles)) {
+           foreach ($roles as $role) {
+               return $this->hasAnyRoles($role);
+           }
+       }
+
+       return $this->roles->contains('name', $roles);
+       
+    }
 }
